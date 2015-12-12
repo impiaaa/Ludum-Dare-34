6a08f73016f807206f8df0152c4ccc571a1cfa32
Spencer Alves <impiaaa@gmail.com>
2015-12-11 22:02:28 -0800

    Box spawning

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxController : MonoBehaviour {
+	public Vector3 velocity;
+	public float dieZ;
+
+	Vector3 initialPosition;
+	float startTime;
+
+	void Start() {
+		startTime = Time.time;
+		initialPosition = transform.position;
+	}
+
+	void Update () {
+		transform.position = initialPosition + velocity * (Time.time - startTime);
+		if (transform.position.z < -dieZ) {
+			Destroy(gameObject);
+		}
+		else if (transform.position.z < 0.0f) {
+			Material m = GetComponent<Renderer>().material;
+			m.color = new Color(m.color.r, m.color.g, m.color.b, 1.0f - transform.position.z / -dieZ);
+		}
+	}
+}
---
+++ Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxSpawner : MonoBehaviour {
+	public float repeatTime;
+	public Transform[] boxPrefabs;
+	public Vector3 spawnSize;
+
+	void Start () {
+		InvokeRepeating("SpawnBox", 1.0f, repeatTime);
+	}
+
+	void SpawnBox() {
+		if (boxPrefabs.Length > 0) {
+			Transform prefabBox = boxPrefabs[Random.Range(0, boxPrefabs.Length)];
+			Vector3 boxSize = prefabBox.GetComponent<MeshFilter>().sharedMesh.bounds.size;
+			Vector3 adjustedSize = Vector3.Max(spawnSize - boxSize, new Vector3());
+			Vector3 position = new Vector3(Random.value*adjustedSize.x, Random.value*adjustedSize.y, Random.value*adjustedSize.z) - (adjustedSize*0.5f);
+			position += transform.position;
+			Instantiate(prefabBox, position, Quaternion.identity);
+		}
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireCube(transform.position, spawnSize);
+	}
+}
